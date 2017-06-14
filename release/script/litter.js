db6dd4c998d8d00b0433a4aee4a9bec7ce95f1df
Razrab <litter.css@gmail.com>
2017-06-14 14:39:36 +0400

    fixed bug

--- release/script/litter.js
+++ release/script/litter.js
@@ -1,7 +1,7 @@
 /*
 Develop: Oleg Chernov
-Site: http://litter-css.ru
-Version: 1.2
+Site: https://litter-css.ru
+Version: 1.3
 */
 
 function CreateElement(id, text, element, idElement) {
@@ -14,66 +14,48 @@ function CreateElement(id, text, element, idElement) {
 	console.log("Item added " + element + " with id " + idElement);
 }
 
-function RemoveElement(id) {
-	let el = document.getElementById(id);
-	el.parentNode.removeChild(el);
-	console.log("Removed item with id " + id);
+function RemoveElement(selector) {
+	let elem = document.querySelector(selector);
+	elem.parentNode.removeChild(elem);
+	console.log("Removed item with selector " + selecto);
 }
 function SearchTag(tag) {
-	let element = document.getElementsByTagName(tag);
-	console.log(element.length);
-	return element.length;
+	let elem = document.getElementsByTagName(tag);
+	console.log(elem.length);
+	return elem.length;
 }
 
 function SearchClass(classT) {
-	let element = document.getElementsByClassName(classT);
-	console.log("class: " + element.length);
-	return element.length;
+	let elem = document.getElementsByClassName(classT);
+	console.log("class: " + elem.length);
+	return elem.length;
 }
 
-function SetText(id, text) {
-	let element = document.getElementById(id);
-	element.innerHTML = text;
+function SetText(selector, text) {
+	let elem = document.querySelector(selector);
+	elem.innerHTML = text;
 }
 
-function AppendText(id, text) {
-	let element = document.getElementById(id);
+function AppendText(selector, text) {
+	let elem = document.querySelector(selector);
 	let text_element = document.createTextNode(text);
-	element.appendChild(text_element);
+	elem.appendChild(text_element);
 	var result = "";
 }
 
-function CreateImg(id, src, width, height, idElement) {
+function CreateImg(id, src, width, height, selector) {
     let elem = document.createElement("img");
     elem.src = src;
     elem.id = id;
     elem.style.width = width;
     elem.style.height = height;
-	let root_element = document.getElementById(id);
+	let root_element = document.querySelector(selector);
 	root_element.appendChild(elem);	
 }
 
-function EditHref(idElement, valueAttr) {
-	let element = document.getElementById(idElement);
-	element.href = valueAttr;
-}
-
-//Testing version
-
-function Dom(root = document.body) {
-// root - элемент в котором будут происходить действия
-	this.root = root;
-	if (root != document.body) {
-		this.root = document.quer
-	}
-
-	this.createElement = functio(element, node) {
-		let text_elem = document.createTextNode(node);
-		elem.appendChild(text_elem);
-		let root_element = document.getElementById(id);
-		root_element.appendChild(elem);
-		elem.id = idElement;
-	}
+function EditHref(selector, valueAttr) {
+	let elem = document.querySelector(selector);
+	elem.href = valueAttr;
 }
 function showModal(idElement, idBackground) {
 	let modal = document.getElementById(idElement); // полчаем id блока
