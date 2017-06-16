0a63288d60be9cf0b634897b4d699ba0dd2f5bae
Marat Nagayev <sidorovmarat1995@gmail.com>
2017-06-16 15:45:08 +0300

    add calc function

--- js/utils.js
+++ js/utils.js
@@ -32,3 +32,53 @@ function randbg(arr,obj,el=arr.length) {
 		let rand = Math.round(Math.random() * el);
 		elem.style.backgroundColor = arr[rand];
 	}
+function calc(str){
+let i,res; let b="err"; 
+let a=""; let c="";
+i=(str.indexOf("+")>=0)?str.indexOf("+"):false; 
+i=(i!=false)?i:str.indexOf("-"); 
+i=(i!=-1)?i:false; 
+i=(i!=false)?i:str.indexOf("*"); 
+i=(i!=-1)?i:false; 
+i=(i!=false)?i:str.indexOf("/");
+i=(i!=-1)?i:false;
+i=(i!=false)?i:str.indexOf("%");
+i=(i!=-1)?i:false;
+i=(i!=false)?i:str.indexOf("^");
+i=(i!=-1)?i:false;
+if(i!=false) b=str[i];
+for(j=0;j<i;j++){
+   a+=str[j]; //1 st number(type is string)
+}
+for(z=i+1;z<=str.length;z++){
+   c+=str[z]; 
+//2 number(type is string)
+}
+a=parseInt(a); //parse 1 st umber
+c=parseInt(c); //parse 2 nd number
+switch(b){
+   case "+":
+   res=a+c;
+   break;
+   case "-":
+   res=a-c;
+   break;
+   case "*":
+   res=a*c;
+   break;
+   case "/":
+   res=a/c;
+   break;
+   case "%":
+   res=a%c;
+   break;
+   case "^":
+   res=Math.pow(a,c);
+   break;
+   default:
+   res="Error";
+}
+delete a,c,b,z,j,i; //delete all vars except res 
+return res;
+}
+
