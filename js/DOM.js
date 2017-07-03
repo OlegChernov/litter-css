495e42451cfc4bece7dafeacb9e0941f6b305ab7
Marat Nagayev <sidorovmarat1995@gmail.com>
2017-07-03 16:20:03 +0300

    not es2015 now

--- js/DOM.js
+++ js/DOM.js
@@ -1,59 +1,64 @@
 /*
 Develop: Oleg Chernov
 Site: https://litter-css.ru
-Version: 1.3
+Version: 2.0
 */
 
 function CreateElement(id, text, element, idElement) {
-	let elem = document.createElement(element);
-	let text_elem = document.createTextNode(text);
+	var elem = document.createElement(element);
+	var text_elem = document.createTextNode(text);
 	elem.appendChild(text_elem);
-	let root_element = document.getElementById(id);
+	var root_element = document.getElementById(id);
 	root_element.appendChild(elem);
 	elem.id = idElement;
 	console.log("Item added " + element + " with id " + idElement);
+    delete elem,text_elem,root_element;
 }
 
 function RemoveElement(selector) {
-	let elem = document.querySelector(selector);
+	var elem = document.querySelector(selector);
 	elem.parentNode.removeChild(elem);
 	console.log("Removed item with selector " + selecto);
 }
 function SearchTag(tag) {
-	let elem = document.getElementsByTagName(tag);
+	var elem = document.getElementsByTagName(tag);
 	console.log(elem.length);
 	return elem.length;
 }
 
 function SearchClass(classT) {
-	let elem = document.getElementsByClassName(classT);
+	var elem = document.getElementsByClassName(classT);
 	console.log("class: " + elem.length);
 	return elem.length;
 }
 
 function SetText(selector, text) {
-	let elem = document.querySelector(selector);
+	var elem = document.querySelector(selector);
 	elem.innerHTML = text;
+    delete elem;
 }
 
 function AppendText(selector, text) {
-	let elem = document.querySelector(selector);
-	let text_element = document.createTextNode(text);
+	var elem = document.querySelector(selector);
+	var text_element = document.createTextNode(text);
 	elem.appendChild(text_element);
 	var result = "";
+     delete elem,text_element;
 }
 
 function CreateImg(id, src, width, height, selector) {
-    let elem = document.createElement("img");
+    var elem = document.createElement("img");
     elem.src = src;
     elem.id = id;
     elem.style.width = width;
     elem.style.height = height;
-	let root_element = document.querySelector(selector);
-	root_element.appendChild(elem);	
+    var root_element = document.querySelector(selector);
+    root_element.appendChild(elem);	
+   delete elem,root_element;
 }
 
 function EditHref(selector, valueAttr) {
-	let elem = document.querySelector(selector);
+	var elem = document.querySelector(selector);
 	elem.href = valueAttr;
+     delete elem;
 }
